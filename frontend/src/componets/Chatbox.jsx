65c775cf1fce6319d14aa2098637b56da2a43b82
Mahendra Soni <msdraj02@protonmail.com>
2023-07-19 04:29:05 +0530

    chatbox started

--- frontend/src/componets/Chatbox.jsx
+++ frontend/src/componets/Chatbox.jsx
@@ -1,7 +1,28 @@
-import React from 'react'
+import React , {useState, useEffect} from 'react'
 
 export default function Chatbox() {
+
+const [messageArray , setMessageArray] = useState(["chup kar madarchod", "thik hai bhai"])
+
+
+function send_message(e){
+    e.preventDefault()
+    setMessageArray(messageArray.concat(e.target.elements.your_message.value))
+    e.target.elements.your_message.value=""
+}
+console.log(messageArray)
   return (
-    <div>Chatbox</div>
+    <>
+    <div className='p-4 absolute right-0 bottom-16' id='chat_feed'>
+  {messageArray.map((data, index) => (
+    <h1 className='bg-blue-400 text-white rounded-lg px-2 py-1 mb-2'>{data}</h1>
+  ))}
+</div>
+
+        <form action="" className='absolute bottom-0 w-full p-4' onSubmit={send_message}>
+        <input type="text" id="your_message" name='your_message'  className='w-4/5 h-9 rounded-md outline-none px-3'/>
+        <button type='submit' className='bg-black rounded-md text-blue-500 h-9 px-3 ml-4 hover:text-white hover:bg-transparent'>Send Message</button>
+        </form>
+    </>
   )
 }
--- frontend/src/componets/Home.jsx
+++ frontend/src/componets/Home.jsx
@@ -5,13 +5,13 @@ import Chatbox from "./Chatbox";
 export default function Home() {
   return (
     <>
-      <div className=" flex  border-red-400 border-4 h-screen justify-center items-center home">
+      <div className=" flex h-screen justify-center items-center home">
         <div className="flex w-4/5 h-4/5 bg-slate-500 bg-opacity-50">
 
           <div className=" w-1/4 p-4 flex flex-col">
                 <Sidename></Sidename>
           </div>
-          <div className="  w-3/4 p-4">
+          <div className="relative w-3/4">
             <Chatbox></Chatbox>
           </div>
         </div>
