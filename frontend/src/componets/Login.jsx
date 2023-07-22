d3f0a2d2ff40df8703ddec6ee9b48156cef6b61c
Mahendra Soni <msdraj02@protonmail.com>
2023-07-22 10:40:56 +0530

    login signup connected

--- frontend/src/componets/Login.jsx
+++ frontend/src/componets/Login.jsx
@@ -1,25 +1,63 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios'
+
+const base_url = "http://localhost:5000/login"
 
 export default function Login() {
+  function loggingIn(e) {
+    e.preventDefault();
+    const temp_object = {
+      email: e.target.elements["email"].value,
+      password: e.target.elements["password"].value,
+    };
+    axios.post(base_url, temp_object).then(function (response) {
+      console.log("Your response", response.data);
+    });
+  }
   return (
     <>
-    <div className='bg-purple-100 h-full '>
-      <div className='p-4 lg:p-10 sm:p-8'>
-     <h1 className='font-bold text-3xl text-black mb-8'>Sign In</h1>
-      <form action="">
-      <input className='block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100' type="text" name="username" id="username" placeholder='Username' />
-      <input className='block w-full border-b-4 border-purple-800 mb-12 outline-none bg-purple-100' type="text" name='password' id='password' placeholder='Password' />
-      <button type='submit' className='block w-full bg-purple-400 py-2 rounded-lg font-semibold text-lg mb-8 hover:ring-4 ring-purple-500 transition duration-300'>Continue</button>
-      </form>
+      <div className="bg-purple-100 h-full ">
+        <div className="p-4 lg:p-10 sm:p-8">
+          <h1 className="font-bold text-3xl text-black mb-8">Sign In</h1>
+          <form action="" onSubmit={loggingIn}>
+            <input
+              className="block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Your email"
+            />
+            <input
+              className="block w-full border-b-4 border-purple-800 mb-12 outline-none bg-purple-100"
+              type="text"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+            <button
+              type="submit"
+              className="block w-full bg-purple-400 py-2 rounded-lg font-semibold text-lg mb-8 hover:ring-4 ring-purple-500 transition duration-300"
+            >
+              Continue
+            </button>
+          </form>
 
-      <p className='text-sm text-black mb-4 text-center'>Don't have a account? <Link to="/signup" className='no-underline text-blue-500'>Sign Up</Link></p>
-      <button className='block w-full bg-blue-300 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-400 transition duration-300'>Google</button>
+          <p className="text-sm text-black mb-4 text-center">
+            Don't have a account?{" "}
+            <Link to="/signup" className="no-underline text-blue-500">
+              Sign Up
+            </Link>
+          </p>
+          <button className="block w-full bg-blue-300 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-400 transition duration-300">
+            Google
+          </button>
 
-      <button className='block w-full bg-blue-500 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-600 transition duration-300'>Facebook</button>
-
+          <button className="block w-full bg-blue-500 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-600 transition duration-300">
+            Facebook
+          </button>
+        </div>
       </div>
-    </div>
     </>
-  )
+  );
 }
--- frontend/src/componets/Signup.jsx
+++ frontend/src/componets/Signup.jsx
@@ -1,33 +1,89 @@
-import React from 'react'
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from 'axios';
 
+const base_url = "http://localhost:5000/register"
 
 export default function Signup() {
+  function signingUp(e){
+    e.preventDefault()
+    const temp_object = {
+      "username":e.target.elements["username"].value,
+      "email":e.target.elements["email"].value,
+      "phone_number":e.target.elements["phone_number"].value,
+      "password":e.target.elements["password"].value,
+    }
+    axios.post(base_url, temp_object)
+    .then(function(response) {
+      console.log("Your response", response.data);
+    })
+  }
   return (
     <>
-    <div className='bg-purple-100 h-full '>
-      <div className='p-4 lg:p-10 sm:p-8'>
-     <h1 className='font-bold text-3xl text-black mb-8'>Sign Up</h1>
-      <form action="">
-      <input className='block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100' type="text" name="username" id="username" placeholder='Username' />
+      <div className="bg-purple-100 h-full ">
+        <div className="p-4 lg:p-10 sm:p-8">
+          <h1 className="font-bold text-3xl text-black mb-8">Sign Up</h1>
 
-      <input className='block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100' type="email" name="email" id="email" placeholder='Email' />
-      <input className='block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100' type="text" name="phone_number" id="phone_number" placeholder='Phone Number' />
+          <form action="" onSubmit={signingUp} name="form_name">
+            <input
+              className="block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100"
+              type="text"
+              name="username"
+              id="username"
+              placeholder="Username"
+            />
 
+            <input
+              className="block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100"
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Email"
+            />
+            <input
+              className="block w-full border-b-4 border-purple-800 mb-4 outline-none bg-purple-100"
+              type="number"
+              name="phone_number"
+              id="phone_number"
+              maxLength = "10"
 
-      <input className='block w-full border-b-4 border-purple-800 mb-12 outline-none bg-purple-100' type="text" name='password' id='password' placeholder='Password' />
-      <button type='submit' className='block w-full bg-purple-400 py-2 rounded-lg font-semibold text-lg mb-8 hover:ring-4 ring-purple-500 transition duration-300'>Continue</button>
-      </form>
-      <p className='text-sm text-black  text-center'>Already have a account? <Link to="/login" className='no-underline text-blue-500'>Sign In</Link></p>
+              placeholder="Phone Number"
+            />
 
-      <p className='font-bold text-sm text-black mb-4 text-center'>Or, Sign Up using</p>
-      <button className='block w-full bg-blue-300 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-400 transition duration-300'>Google</button>
+            <input
+              className="block w-full border-b-4 border-purple-800 mb-12 outline-none bg-purple-100"
+              type="text"
+              name="password"
+              id="password"
+              placeholder="Password"
+            />
+            <button
+              type="submit"
+              className="block w-full bg-purple-400 py-2 rounded-lg font-semibold text-lg mb-8 hover:ring-4 ring-purple-500 transition duration-300"
+            >
+              Continue
+            </button>
+          </form>
 
-      <button className='block w-full bg-blue-500 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-600 transition duration-300'>Facebook</button>
+          <p className="text-sm text-black  text-center">
+            Already have a account?{" "}
+            <Link to="/login" className="no-underline text-blue-500">
+              Sign In
+            </Link>
+          </p>
 
+          <p className="font-bold text-sm text-black mb-4 text-center">
+            Or, Sign Up using
+          </p>
+          <button className="block w-full bg-blue-300 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-400 transition duration-300">
+            Google
+          </button>
+
+          <button className="block w-full bg-blue-500 py-2 font-semibold text-lg mb-8 hover:ring-4 ring-blue-600 transition duration-300">
+            Facebook
+          </button>
+        </div>
       </div>
-    </div>
     </>
- 
-  )
+  );
 }
