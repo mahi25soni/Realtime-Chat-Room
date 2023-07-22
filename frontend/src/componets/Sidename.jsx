034b378d053b6c602c11765e3058494eaf659c47
Mahendra Soni <msdraj02@protonmail.com>
2023-07-22 09:16:13 +0530

    add room option

--- frontend/src/componets/Sidename.jsx
+++ frontend/src/componets/Sidename.jsx
@@ -3,11 +3,22 @@ import React from 'react'
 export default function Sidename() {
   return (
     <>
+  <div className='mb-4 text-center'>
+    <label for="add_genre"></label>
+            <select name="add_genre" id="add_genre" className='rounded-md text-center mt-2 py-1 bg-blue-500 text-black text-black-500 font-bold text-sm'>
+              <option value="blockchain">Blockchain</option>
+              <option value="artificial_intel">Artificial Intelligance</option>
+              <option value="mercedes">Mercedes</option>
+              <option value="audi">Audi</option>
+            </select>
+        <button type='submit' className='bg-black rounded-md text-blue-500  px-3 ml-4 hover:text-white hover:bg-transparent'>Add</button>
+
+    </div>
     <div className='text-center'>
     <label for="your_genre" className='mb-4  text-center font-bold text-2xl'>Choose Your Room:</label>
-            <select name="your_genre" id="your_genre" className='rounded-md text-center mt-2 bg-transparent text-black-500 font-bold'>
+            <select name="your_genre" id="your_genre" className='rounded-md text-center mt-2 bg-transparent text-black-500 font-bold font-lg'>
               <option value="blockchain">Blockchain</option>
-              <option value="artificial_intel">Artificial Interlligance</option>
+              <option value="artificial_intel">Artificial Intelligance</option>
               <option value="mercedes">Mercedes</option>
               <option value="audi">Audi</option>
             </select>
