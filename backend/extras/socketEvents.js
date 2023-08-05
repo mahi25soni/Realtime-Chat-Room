533377268fd53f8293e7f8ef5fb0090ead8c13ab
Mahendra Soni <msdraj02@protonmail.com>
2023-08-05 23:39:08 +0530

    nothign

--- backend/extras/socketEvents.js
+++ backend/extras/socketEvents.js
@@ -1,13 +1,28 @@
+const axios = require("axios")
+const base_url = "http://127.0.0.1:5000/chatroom/message";
+
 let totalUsers = 0;
 const socket_connection = (io) => {
     io.on('connection' , (socket) => {
         totalUsers++;
-        console.log(`New user connected. Total users: ${totalUsers}`);
+        console.log(`After connecting`, totalUsers)
+        // socket.on("join_room", (room_id) => {
+        //     socket.join(room_id)
+        //     console.log("User id number ", socket.id, " is room id ", room_id)
+        // })
         socket.on("send_message", (data) => {
-            console.log(socket.id)
-
-            console.log("something I don't want to share ", data)
-            io.emit("get_message", data)
+            io.emit("get_message", data.new_message)
+            axios.post(`${base_url}/${data.current_room}`, {
+                "message" : data.new_message
+            }, {
+                headers : {
+                    authorization : data.auth_token
+                }
+            })
+            .then((response) => console.log(response.data))
+              .catch(error => {
+                console.log("Error: ", error);
+              });
         })
         socket.on("disconnect", () =>  {
             totalUsers--;
